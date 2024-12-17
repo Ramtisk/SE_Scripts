722ade0075698342889def96d43399c6a08a6786
Ramtisk <97610281+Ramtisk@users.noreply.github.com>
2024-12-17 21:30:40 +0000

    Delete Replika360 directory

--- Replika360/app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_pymongo import PyMongo
-
-app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/meshroom_projects"
-mongo = PyMongo(app)
-
-from app import routes
